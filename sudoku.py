dd9d3b7e932919182a215b525254646d67cb9b13
Conor McAvoy <cgm26@duke.edu>
2018-05-07 17:57:11 -0400

    Add printing capabilities

--- sudoku.py
+++ sudoku.py
@@ -15,6 +15,9 @@ class Row:
     def __init__(self, numbers):
         self.numbers = [n for n in numbers]
 
+    def __str__(self):
+        return ''.join(self.numbers)
+
 class Box:
     def __init__(self, first_three):
         self.numbers = [n for n in first_three]
@@ -28,6 +31,9 @@ class Grid:
         self.rows = []
         self.boxes = []
 
+    def __str__(self):
+        return '\n'.join(str(row) for row in self.rows)
+
     def add_cols(self, cols):
         self.cols = cols
 
@@ -37,13 +43,14 @@ class Grid:
     def add_boxes(self, boxes):
         self.boxes.extend(boxes)
 
+
+
 def main():
     parser = argparse.ArgumentParser(description="Solve sudoku puzzles.")
     parser.add_argument("filename", help="name of location of sudoku file")
 
     args = parser.parse_args()
     filename = args.filename
-
 
     g = Grid()
     with open(filename, 'r') as f:
@@ -57,17 +64,13 @@ def main():
         col7 = Col()
         col8 = Col()
         col9 = Col()
+        all_cols = [col1, col2, col3, col4, col5, col6, col7, col8, col9]
         
         for line in f:
-            col1.add(line[0])
-            col2.add(line[1])
-            col3.add(line[2])
-            col4.add(line[3])
-            col5.add(line[4])
-            col6.add(line[5])
-            col7.add(line[6])
-            col8.add(line[7])
-            col9.add(line[8])
+            i = 0
+            for col in all_cols:
+                col.add(line[i])
+                i += 1
             
             row = Row(line[0:9])
             g.add_row(row)
@@ -86,7 +89,9 @@ def main():
 
             line_count += 1
                 
-        g.add_cols([col1, col2, col3, col4, col5, col6, col7, col8, col9])
+        g.add_cols(all_cols)
+
+    print(g)
             
 
 
